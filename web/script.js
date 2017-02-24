8d9a35bc762123689325be26d9129f65ba6718c7
Sm0kersneverdie <saugata@metropolia.com>
2017-02-24 11:21:17 +0200

    Update order taking functionality and tables

--- web/script.js
+++ web/script.js
@@ -4,6 +4,15 @@
  * and open the template in the editor.
  */
 $(document).ready(function () {
+    $('#newTasks').hide();
+    $('#addNew').hide();
+    /*Navbar functions */
+    $('#takeOrder').click(function (event) {
+        event.preventDefault();
+        $('#newTasks').show();
+        $('#addNew').show();
+    });
+    /*End of Navbar functions */
 
     //Add button click event
     $('#addNew').click(function () {
@@ -22,10 +31,13 @@ $(document).ready(function () {
     });
 
     function addNewRow() {
-        var numRows = $('#newTasks tr').length;
-
-        $('#newTasks').append('<tr><td><input type="text" id="title-' + numRows + '" /></td><td><input type="text" id="orders-' + numRows + '" /></td></tr>');
-
+//        var numRows = $('#newTasks tr').length;
+//
+//        $('#newTasks').append('<tr><td><input type="text" id="title-' + numRows + '" /></td><td><input type="text" id="orders-' + numRows + '" /></td><td><input type="text" id="status-'+numRows+'" /></td></tr>');
+        refreshNotes();
+        $('#newTasks').find("input").val("");
+        $('#newTasks').hide();
+        $('#addNew').hide();
     }
 
     /**
@@ -34,33 +46,35 @@ $(document).ready(function () {
      */
     function refreshNotes() {
 
-        var tableRows = $('#newTasks tr');
 
-        $('.sticky_notes li').remove();
+        /* Table functions */
+        var tableRows = $('#newTasks');
+        console.log(tableRows);
+        var title = tableRows.find('input[id^="table"]').val();
+        var orders = tableRows.find('input[id^="orders"]').val();
+        var status = tableRows.find('input[id^="status"]').val();
+        if (title && status && orders) {
+            createNotes(title, orders, status);
+        }
 
-        $.each(tableRows, function (i) {
-            var title = $(this).find('input[id^="title"]').val();
-            var orders = $(this).find('input[id^="orders"]').val();
-            var status = $(this).find('input[id^="status"]').val();
-            console.log("hello");
-            if (title != undefined && orders != undefined && status != undefined) {
-                createNotes(title, orders, status);
-            }
-        });
+        console.log(title);
+
     }
-
+    /*End of table functions */
     /**
      * Creates the sticky notes and gives it a random colour.
      */
+
+    /*Note functions */
     function createNotes(title, orders, status) {
         var header = '<h4>Table No.' + title + '</h4>';
         var desc = '<p>Orders: ' + orders + '</p>';
         var statuses = '<p>Status: ' + status + '</p>';
         var d = new Date(); // for now
-         
-        
-       
-        var time = '<p>Time: ' +d.getHours()+":"+d.getMinutes()+":"+d.getSeconds()+'</p>'
+
+
+
+        var time = '<p>Time: ' + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds() + '</p>'
 
         var colours = new Array();
         colours[0] = 'green';
@@ -71,7 +85,7 @@ $(document).ready(function () {
         colours[5] = 'orange';
 
 
-        $('.sticky_notes').append('<li class="' + colours[randomFromTo(0, (colours.length - 1))] + '">' + header + desc + statuses + time+'</li>');
+        $('.sticky_notes').append('<li class="' + colours[randomFromTo(0, (colours.length - 1))] + '">' + header + desc + statuses + time + '</li>');
     }
 
     /**
@@ -87,12 +101,13 @@ $(document).ready(function () {
      * Deletes the grandparent of the delete button
      */
     function deleteRow(thisButton) {
-        thisButton.parent().parent().remove();
+        $('#newTasks').find("input").val("");
     }
 
 });
-
+/*End of Note functions */
 /**
  * Click event function to append a new row to the tasks table
  */
+
 
